ba3e1ef5ce793a6b2394e48a78b7ece80c8f4490
Kevin Lane <klanemecrash@gmail.com>
2018-06-05 19:49:26 +0000

    places identified by users

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,7 @@
 class Place < ApplicationRecord
+	validates :name, presence: true, length: {maximum: 200, minimum: 3}
+	validates :address, presence: true, length: { maximum: 200, minimum: 10 }
+
+	belongs_to :user
+
 end
